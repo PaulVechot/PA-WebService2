6e4b86d9240ad8aa71ce8b2eb8dc966c09adeada
PaulVechot <paul.vechot@gmail.com>
2018-09-05 22:58:49 +0200

    configurations_set post

--- routes/analysis_result.js
+++ routes/analysis_result.js
@@ -1,12 +1,13 @@
 'use strict';
 
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const controllers = require('../controllers');
 const Analysis_resultController = controllers.Analysis_resultController;
 
 const Analysis_resultRouter = express.Router();
-//Analysis_resultRouter.use(bodyParser.json());
+
+Analysis_resultRouter.use(bodyParser.json());
 
 Analysis_resultRouter.get('/', function(req, res) {
     Analysis_resultController.getAll()
@@ -19,6 +20,16 @@ Analysis_resultRouter.get('/', function(req, res) {
     });
 });
 
+Analysis_resultRouter.get('/:resultId', function(req, res) {
+    Analysis_resultController.getStatsFor(req.params.resultId)
+    .then((stats) => {
+        res.json(stats);
+    })
+    .catch((err) => {
+        console.error(err);
+        res.status(500).end();
+    });
+});
 Analysis_resultRouter.get('/:resultId', function(req, res) {
     Analysis_resultController.getStatsFor(req.params.resultId)
     .then((stats) => {
--- routes/configuration_set.js
+++ routes/configuration_set.js
@@ -1,12 +1,12 @@
 'use strict';
 
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const controllers = require('../controllers');
 const ConfiguationController = controllers.ConfiguationController;
 
 const ConfiguationRouter = express.Router();
-//ConfiguationRouter.use(bodyParser.json());
+ConfiguationRouter.use(bodyParser.json());
 
 ConfiguationRouter.get('/', function(req, res) {
     ConfiguationController.getAll()
@@ -29,5 +29,22 @@ ConfiguationRouter.get('/:resultId', function(req, res) {
         res.status(500).end();
     });
 });
+
+ConfiguationRouter.post('/', function(req, res) {
+  const id = req.body.id;
+  const label = req.body.label;
+  if(id === undefined || label === undefined) {
+    res.status(400).end();
+    return;
+  }
+  TaskController.add(parseInt(id), label)
+  .then((configuration_set) => {
+    res.status(201).json(configuration_set);
+  })
+  .catch((err) => {
+    res.status(500).end();
+  })
+});
+
 
 module.exports = ConfiguationRouter;
